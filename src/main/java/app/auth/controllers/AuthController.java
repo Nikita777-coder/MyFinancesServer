40e60ddbaa7da352d8f77014d4e8db26306dc166
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-02-09 13:56:42 +0300

    change api paths

--- src/main/java/app/auth/controllers/AuthController.java
+++ src/main/java/app/auth/controllers/AuthController.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
--- src/main/java/app/auth/controllers/UserController.java
+++ src/main/java/app/auth/controllers/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
